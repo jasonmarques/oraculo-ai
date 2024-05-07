8e666b32f3995618b337d9bd5ccfbd5b467fdf2f
jasonmarques <132834740+jasonmarques@users.noreply.github.com>
2024-05-07 19:55:43 -0300

    first commit

---
+++ src/main/java/com/oraculo/ai/oraculoai/OraculoAiApplication.java
@@ -0,0 +1,13 @@
+package com.oraculo.ai.oraculoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OraculoAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OraculoAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oraculo/ai/oraculoai/OraculoAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oraculo.ai.oraculoai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OraculoAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
